016baff8eda465f77958a45d89e3e19d2fb2d046
LATIFARYES <latifaryes@gmail.com>
2024-01-23 22:54:15 +0300

    Initial Commit

---
+++ Progress Bar 100/Progress_Bar_100App.swift
@@ -0,0 +1,17 @@
+//
+//  Progress_Bar_100App.swift
+//  Progress Bar 100
+//
+//  Created by LATIFA on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Progress_Bar_100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
